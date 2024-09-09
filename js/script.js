d934de807b81513583de7ee18082950c02e99095
kezia_b_n <keziabernard2@gmail.com>
2024-09-09 02:04:14 -0400

    Added the start of if/else statement.

--- js/script.js
+++ js/script.js
@@ -3,5 +3,8 @@ let button = document.querySelector(".show-cat");
 let cat = document.querySelector(".cat");
 
 button.addEventListener("click", function(){
-    
+    if (cat.classList.contains("show"))
+    {console.log("Yes")}
+    else 
+    {console.log("No")}
 });
