3d5db82792f5472742473c63d11d20863c6b9829
kezia_b_n <keziabernard2@gmail.com>
2024-09-10 19:15:35 -0400

    Added styling to the button when pushed

--- js/script.js
+++ js/script.js
@@ -4,12 +4,13 @@ let cat = document.querySelector(".cat");
 
 button.addEventListener("click", function(){
     if (cat.classList.contains("show")) {
-        (cat.classList.remove("show"))
+        cat.classList.remove("show")
         document.getElementById("button").innerText = "Wait, come back!"
-        button.style.color = "turqoise"
+        button.style.color = "turquoise"
     }
-    else {(cat.classList.add("show")) 
+    else {cat.classList.add("show")
         document.getElementById("button").innerText = "Shoo cat!"
+        button.style.color = "green"
     }
     
 });
